2bdfb62c7f2aa5130691132366f88b70f0870056
Iamneo <techadmin@iamneo.ai>
2023-08-05 09:26:27 +0000

    Backup Commit

--- reactapp/src/functions/destructuring.js
+++ reactapp/src/functions/destructuring.js
@@ -1,13 +1,16 @@
+// File: reactapp/src/functions/destructuring.js
+
+// Function to get user IP and name using destructuring
 function getUserIpDetails(userResponse = {}) {
-    const {userIp = '0.0.0.0', userName} = userResponse;
+    const { userIp = '0.0.0.0', userName } = userResponse;
     return [userName, userIp];
   }
   
-  
+  // Function to get top three student marks using destructuring
   function getTopThree(studentMarks = []) {
     const [firstMark = null, secondMark = null, thirdMark = null] = studentMarks;
-    return [firstMark, secondMark, thirdMark].sort();
+    return [firstMark, secondMark, thirdMark].sort((a, b) => b - a).slice(0, 3);
   }
   
+  export { getUserIpDetails, getTopThree };
   
-  export { getUserIpDetails,getTopThree};
